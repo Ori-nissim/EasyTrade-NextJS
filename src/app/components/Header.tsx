f00ee33fe2c0055d470be03dece82fafb55a6456
Ori Nissim <orini@matrix.co.il>
2024-10-23 11:55:39 +0300

    started auth process

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -8,14 +8,27 @@ export default function Header() {
     const [isMenuShown, setMenuShown] = useState(false);
     const [isDarkMode, setDarkMode] = useState(false);
 
+    // On initial render, check for saved theme preference in localStorage
+    useEffect(() => {
+        const savedTheme = localStorage.getItem("theme");
+        console.log(savedTheme)
+        if (savedTheme === "dark") {
+            setDarkMode(true);
+        } else {
+            setDarkMode(false);
+        }
+    }, []); // Runs once on component mount
+
     // Effect to set the body class based on the dark mode state
     useEffect(() => {
         if (isDarkMode) {
             document.body.classList.add("dark");
             document.body.classList.remove("light");
+            localStorage.setItem("theme", "dark");
         } else {
             document.body.classList.add("light");
             document.body.classList.remove("dark");
+            localStorage.setItem("theme", "light");
         }
     }, [isDarkMode]); // Runs effect whenever isDarkMode changes
 
@@ -29,13 +42,17 @@ export default function Header() {
     };
 
     return (
-        <nav className="bg-card shadow-xl px-4 py-3 ">
+        <nav className="bg-card shadow-xl px-4 py-3">
             <div className="container mx-auto flex justify-between items-center">
                 <div className="flex items-center">
                     <div className="bg-card p-2 rounded-md">
-                        <img className="h-12" src="https://play-lh.googleusercontent.com/I8NUyhq9CVcHWr4tPkujRk2eDHN9Yr2yIg0VorjzeDYYiW7vtzJ4aoKlIoFNDSXrDjM=w240-h480-rw" alt="EasyTrade Logo" />
+                        <img
+                            className="h-12"
+                            src="https://play-lh.googleusercontent.com/I8NUyhq9CVcHWr4tPkujRk2eDHN9Yr2yIg0VorjzeDYYiW7vtzJ4aoKlIoFNDSXrDjM=w240-h480-rw"
+                            alt="EasyTrade Logo"
+                        />
                     </div>
-                    <h1 className="text-2xl text-text text-gray-800 inter font-bold ">EasyTrade</h1>
+                    <h1 className="text-2xl text-text text-gray-800 inter font-bold">EasyTrade</h1>
                 </div>
                 <ul className="space-x-4 hidden md:flex">
                     <li><a className="text-lg text-text hover:text-textHover" href="/">Home</a></li>
@@ -57,9 +74,9 @@ export default function Header() {
                     <li><a className="rounded-md bg-complement text-white px-5 py-2 font-normal hover:bg-complementHover" href="/log-in">Log In</a></li>
                     <li><a className="border-text text-text border-2 rounded-md py-2 px-4 hover:text-complementHover hover:border-complementHover" href="/sign-up">Sign Up</a></li>
                 </ul>
-                <BurgerMenu  toggleMenu={toggleMenu} />
+                <BurgerMenu toggleMenu={toggleMenu} />
             </div>
-            {isMenuShown && <DropDownMenu isDarkMode={isDarkMode} toggleDarkMode={toggleDarkMode}/>}
+            {isMenuShown && <DropDownMenu isDarkMode={isDarkMode} toggleDarkMode={toggleDarkMode} />}
         </nav>
     );
 }
